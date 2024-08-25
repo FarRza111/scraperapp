29a4a88b5d3a05ac80c15a07543332b3daf635b7
Techie <149437995+FarRza111@users.noreply.github.com>
2024-08-25 19:04:03 +0300

    Update IBAN_standard_scraper.py

--- IBAN_standard_scraper.py
+++ IBAN_standard_scraper.py
@@ -13,50 +13,77 @@ country_urls = [
 'https://bank.codes/iban/structure/new-zealand', 'https://bank.codes/iban/structure/oman', 'https://bank.codes/iban/structure/pakistan', 'https://bank.codes/iban/structure/panama', 'https://bank.codes/iban/structure/pitcairn', 'https://bank.codes/iban/structure/peru', 'https://bank.codes/iban/structure/philippines', 'https://bank.codes/iban/structure/palau', 'https://bank.codes/iban/structure/papua-new-guinea', 'https://bank.codes/iban/structure/poland', 'https://bank.codes/iban/structure/puerto-rico', "https://bank.codes/iban/structure/korea,-democratic-people's-republic-of", 'https://bank.codes/iban/structure/portugal', 'https://bank.codes/iban/structure/paraguay', 'https://bank.codes/iban/structure/palestine,-state-of', 'https://bank.codes/iban/structure/french-polynesia', 'https://bank.codes/iban/structure/qatar', 'https://bank.codes/iban/structure/réunion', 'https://bank.codes/iban/structure/romania', 'https://bank.codes/iban/structure/russian-federation', 'https://bank.codes/iban/structure/rwanda', 'https://bank.codes/iban/structure/saudi-arabia', 'https://bank.codes/iban/structure/sudan', 'https://bank.codes/iban/structure/senegal', 'https://bank.codes/iban/structure/singapore', 'https://bank.codes/iban/structure/south-georgia-and-the-south-sandwich-islands', 
 'https://bank.codes/iban/structure/saint-helena,-ascension-and-tristan-da-cunha', 'https://bank.codes/iban/structure/svalbard-and-jan-mayen', 'https://bank.codes/iban/structure/solomon-islands', 'https://bank.codes/iban/structure/sierra-leone', 'https://bank.codes/iban/structure/el-salvador', 'https://bank.codes/iban/structure/san-marino', 'https://bank.codes/iban/structure/somalia', 'https://bank.codes/iban/structure/saint-pierre-and-miquelon', 'https://bank.codes/iban/structure/serbia', 'https://bank.codes/iban/structure/south-sudan', 'https://bank.codes/iban/structure/sao-tome-and-principe', 'https://bank.codes/iban/structure/suriname', 'https://bank.codes/iban/structure/slovakia', 'https://bank.codes/iban/structure/slovenia', 'https://bank.codes/iban/structure/sweden', 'https://bank.codes/iban/structure/eswatini', 'https://bank.codes/iban/structure/sint-maarten-(dutch-part)', 'https://bank.codes/iban/structure/seychelles', 'https://bank.codes/iban/structure/syrian-arab-republic', 'https://bank.codes/iban/structure/turks-and-caicos-islands', 'https://bank.codes/iban/structure/chad', 'https://bank.codes/iban/structure/togo', 'https://bank.codes/iban/structure/thailand', 'https://bank.codes/iban/structure/tajikistan', 'https://bank.codes/iban/structure/tokelau', 'https://bank.codes/iban/structure/turkmenistan', 'https://bank.codes/iban/structure/timor-leste', 'https://bank.codes/iban/structure/tonga', 'https://bank.codes/iban/structure/trinidad-and-tobago', 'https://bank.codes/iban/structure/tunisia', 'https://bank.codes/iban/structure/türkiye', 'https://bank.codes/iban/structure/tuvalu', 'https://bank.codes/iban/structure/taiwan,-province-of-china', 'https://bank.codes/iban/structure/tanzania,-united-republic-of', 'https://bank.codes/iban/structure/uganda', 'https://bank.codes/iban/structure/ukraine', 'https://bank.codes/iban/structure/united-states-minor-outlying-islands', 'https://bank.codes/iban/structure/uruguay', 'https://bank.codes/iban/structure/united-states', 'https://bank.codes/iban/structure/uzbekistan', 'https://bank.codes/iban/structure/holy-see-(vatican-city-state)', 'https://bank.codes/iban/structure/saint-vincent-and-the-grenadines', 'https://bank.codes/iban/structure/venezuela,-bolivarian-republic-of', 'https://bank.codes/iban/structure/virgin-islands,-british', 'https://bank.codes/iban/structure/virgin-islands,-u.s.', 'https://bank.codes/iban/structure/viet-nam', 'https://bank.codes/iban/structure/vanuatu', 'https://bank.codes/iban/structure/wallis-and-futuna', 'https://bank.codes/iban/structure/samoa', 'https://bank.codes/iban/structure/yemen', 'https://bank.codes/iban/structure/south-africa', 'https://bank.codes/iban/structure/zambia', 'https://bank.codes/iban/structure/zimbabwe']
     
-    
 
-# Define the request headers
-headers = {'User-Agent': 'Mozilla/5.0'}
+import re
+import pycountry
+import urllib.request
+import pandas as pd
+from typing import List, Dict
+from bs4 import BeautifulSoup
 
-data = []
+country_urls = [
 
-for url in country_urls:
+    'https://bank.codes/iban/structure/estonia', 'https://bank.codes/iban/structure/azerbaijan',
+]
+
+# Function to get the soup object
+def get_soup(url: str, headers: Dict[str, str]) -> BeautifulSoup:
     try:
         request = urllib.request.Request(url, headers=headers)
-    
         with urllib.request.urlopen(request) as response:
-            if response.getcode() == 404:
-                continue
-                html_content = response.read()
             html_content = response.read()
-        soup = BeautifulSoup(html_content, 'html.parser')
-    
-        country_name = url.split('/')[-2].capitalize()
-        tables = soup.find_all('table')
-        for table in tables:
-            rows = table.find_all('tr')
-            country_data = {'Country': country_name}
-            for row in rows:
-                cols = row.find_all('td')
-                if not cols:
-                    continue
-   
-                label = cols[0].text.strip()  # The label (e.g., "IBAN Checksum")
-                value = cols[2].text.strip()  # The value (e.g., "14")
-                country_data[label] = value  # Add to the country data dictionary
-    
-            data.append(country_data)  
+        return BeautifulSoup(html_content, 'html.parser')
     except urllib.error.HTTPError as e:
         if e.code == 404:
-            continue
+            print(f"404 Error for URL: {url}")
+            return None
         else:
             raise
     except Exception as e:
-        print(f'this is error type:{e}')
+        print(f"Error fetching {url}: {e}")
+        return None
 
-df = pd.DataFrame(data)
-print(df)
+# Function to process the soup object and extract data
+def process_soup(soup: BeautifulSoup, country_name: str) -> List[Dict[str, str]]:
+    
+    data = []
+    if not soup:
+        return data
 
-df.to_excel('account_info_details.xlsx', index=False)
+    tables = soup.find_all('table')
+    for table in tables:
+        rows = table.find_all('tr')
+        country_data = {'Country': country_name}
+        for row in rows:
+            cols = row.find_all('td')
+            if not cols:
+                continue
+            label = cols[0].text.strip()  # The label (e.g., "IBAN Checksum")
+            value = cols[2].text.strip()  # The value (e.g., "14")
+            country_data[label] = value  # Add to the country data dictionary
+        data.append(country_data)  
+
+    spans = soup.find_all('span')
+    extracted_values = [span.get_text() for span in spans]
+    filtered_list = list(filter(None, extracted_values))
+
+    return data
+
+
+# Define the request headers
+headers = {'User-Agent': 'Mozilla/5.0'}
+all_data = []
+
+for url in country_urls:
+    country_name = url.split('/')[-2].capitalize()
+    soup = get_soup(url, headers)
+    country_data = process_soup(soup, country_name)
+    all_data.extend(country_data)
+
+df = pd.DataFrame(all_data)
+
+# Save the DataFrame to an Excel file
+df.to_excel('account__s.xlsx', index=False)
+
 
 
